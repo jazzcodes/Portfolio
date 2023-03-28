484eb70a58506890a5a02f1dfdfc32832b86502e
jazzcodes <jasleenbhambra01@gmail.com>
2023-03-28 20:07:36 +0530

    menu mobile

--- js/main.js
+++ js/main.js
@@ -9,3 +9,61 @@ collapsibles.forEach((item) =>
 //   const el = document.getElementsByClassName("page");
 //   el.style.height = window.innerHeight + 'px';
 // })();
+
+// const menu= document.querySelector(".nav-vertical");
+// const menuItems = document.querySelectorAll(".nav-vertical__item");
+// const hamburger= document.querySelector(".hamburger");
+// const closeIcon= document.querySelector(".closeIcon");
+// const menuIcon = document.querySelector(".open");
+
+// function toggleMenu() {
+//   if (menu.classList.contains("showMenu")) {
+//     menu.classList.remove("showMenu");
+//     closeIcon.style.display = "none";
+//     menuIcon.style.display = "block";
+//   } else {
+//     menu.classList.add("showMenu");
+//     closeIcon.style.display = "block";
+//     menuIcon.style.display = "none";
+//   }
+// }
+
+// hamburger.addEventListener("click", toggleMenu);
+
+// menuItems.forEach( 
+//   function(menuItem) { 
+//     menuItem.addEventListener("click", toggleMenu);
+//   }
+// )
+
+
+const menu = document.querySelector(".vertical-menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger= document.querySelector(".hamburger");
+const closeIcon= document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+ 
+  
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+    closeIcon.style.maxWidth="100%";
+    closeIcon.style.opacity="1";
+   
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+menuItems.forEach( 
+  function(menuItem) { 
+    menuItem.addEventListener("click", toggleMenu);
+  }
+)
